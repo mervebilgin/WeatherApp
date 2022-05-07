76c56d6579d8f408fc8c90a27c6bac2bc9900d96
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2022-05-07 21:01:44 +0300

    Complete the app

---
+++ script.js
@@ -0,0 +1,36 @@
+const url = 'https://api.openweathermap.org/data/2.5/'
+const key = '739ef515e3ade2c79525a82f51f458a5'
+
+const setQuery = (e) => {
+	if(e.keyCode == '13') 
+		getResult(searchBar.value)
+	
+}
+
+const getResult = (cityName) => { 
+	let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+	
+	fetch(query)
+	.then(weather => {
+		return weather.json()
+	})
+	.then(displayResult)
+} 
+
+const displayResult = (result) => {
+
+	let city = document.querySelector('.city')
+	city.innerText = `${result.name}, ${result.sys.country}`
+
+	let temp = document.querySelector('.temp')
+	temp.innerText = `${Math.round(result.main.temp)}°C`
+
+	let desc = document.querySelector('.desc')
+	desc.innerText = result.weather[0].description
+
+	let minmax = document.querySelector('.minmax')
+	minmax.innerText = `${Math.round(result.main.temp_min)}°C /${Math.round(result.main.temp_max)}°C`
+}
+
+const searchBar = document.getElementById('searchBar')
+searchBar.addEventListener('keypress',setQuery)
